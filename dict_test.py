a55f2d8817e4e0b388f86342fa71bb79b799abb1
li xueao <1096183069@qq.com>
2023-03-28 09:37:15 +0800

    modified:   .gitignore 	modified:   app.py 	modified:   dict_test.py 	modified:   templates/row.html

--- dict_test.py
+++ dict_test.py
@@ -1,2 +1,18 @@
-projects = {"default": {"name": "default", "title": 'title','uid_key': 'id','database': 'db','search_template': 'ase/db/templates/search.html','search_template': 'ase/db/templates/search.html','row_template': 'ase/db/templates/row.html','table_template': 'ase/db/templates/table.html'}}
-print(projects["default"])
+'''
+Author: Xueao Li @ DUT
+Date: 2023-02-12 15:17:34
+LastEditors: Xueao Li @ DUT
+LastEditTime: 2023-03-27 13:57:50
+Description: The description of this script.
+
+'''
+#projects = {"default": {"name": "default", "title": 'title','uid_key': 'id','database': 'db','search_template': 'ase/db/templates/search.html','search_template': 'ase/db/templates/search.html','row_template': 'ase/db/templates/row.html','table_template': 'ase/db/templates/table.html'}}
+#print(projects["default"])
+
+from ase.db import connect
+
+db = connect("As.db")
+
+for row in db.select():
+    db.update(row.id, similarity="[1,2,3]")
+
